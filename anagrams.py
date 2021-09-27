ea7944762178e92b3efa15074f3b953a0e3213e3
GMCosmin1 <91424622+GMCosmin1@users.noreply.github.com>
2021-09-27 12:38:40 +0300

    Modify anagrams

--- anagrams.py
+++ anagrams.py
@@ -2,14 +2,14 @@
 
 def check_anagrams(s1, s2):
 
-    if(sorted(s1) == sorted(s2)):
+    if(sorted(s1.lower()) == sorted(s2.lower())):
         print(s1 + " and " + s2 + " are anagrams.")
     else:
         print(s1 + " and " + s2 + " aren't anagrams.")        
 
 
 s1 = "listen"
-s2 = "silent"
+s2 = "sileNt"
 
 s3 = "aaaaa"
 s4 = "bbbbb"
